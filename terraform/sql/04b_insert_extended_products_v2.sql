14a8c8f3a900ca5459ebf98575e5e218dae47eb0
Lorenzo Nicora <lnicora@confluent.io>
2026-04-21 13:05:27 +0200

    Fix statement v2

--- terraform/sql/04b_insert_extended_products_v2.sql
+++ terraform/sql/04b_insert_extended_products_v2.sql
@@ -3,5 +3,5 @@ INSERT INTO `extended_products`
 SELECT
     product_id,
     `name`,
-    concat_with_separator(`name`, `brand`, `vendor`, `brand`, ' + ') AS extended_name
+    concat_with_separator(`name`, `brand`, `vendor`, `department`, ' + ') AS extended_name
 FROM `base_products`%{ if initial_offsets != "" } /*+ OPTIONS('scan.startup.mode' = 'specific-offsets', 'scan.startup.specific-offsets' = '${initial_offsets}') */%{ endif };
